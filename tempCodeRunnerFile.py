64fdbc722101ac09d4eaea7b92816661a94078d7
Muhammad Abdullah <113567773+AbdullahAssi@users.noreply.github.com>
2024-01-18 22:43:23 +0500

    progress

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+#to store the results of each frame
--- util.py
+++ util.py
@@ -116,6 +116,16 @@ def read_license_plate(license_plate_crop):
         tuple: Tuple containing the formatted license plate text and its confidence score.
     """
 
+    detections = reader.readtext(license_plate_crop)
+
+    for detection in detections:
+        bbox, text, score = detection
+
+        text = text.upper().replace(' ', '')
+
+        if license_complies_format(text):
+            return format_license(text), score
+
     return None, None
 
 
@@ -130,6 +140,18 @@ def get_car(license_plate, vehicle_track_ids):
     Returns:
         tuple: Tuple containing the vehicle coordinates (x1, y1, x2, y2) and ID.
     """
-    
+    x1, y1, x2, y2, score, class_id = license_plate
 
-    return 0,0,0,0,0
+    foundIt = False
+    for j in range(len(vehicle_track_ids)):
+        xcar1, ycar1, xcar2, ycar2, car_id = vehicle_track_ids[j]
+
+        if x1 > xcar1 and y1 > ycar1 and x2 < xcar2 and y2 < ycar2:
+            car_indx = j
+            foundIt = True
+            break
+
+    if foundIt:
+        return vehicle_track_ids[car_indx]
+
+    return -1, -1, -1, -1, -1
