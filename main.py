f14abdf6f6e8cbb57806ca1177bc95fcf92c3eae
Muhammad Abdullah <113567773+AbdullahAssi@users.noreply.github.com>
2024-01-18 21:20:20 +0500

    First one

---
+++ main.py
@@ -0,0 +1,21 @@
+# load models
+
+# load video
+
+# read frames
+
+# detect vehicles
+
+# track vehicles
+
+# detect license plates
+
+# assign license plate to car
+
+# crop license plate
+
+# process license plate
+
+# read license plate number
+
+# write results
